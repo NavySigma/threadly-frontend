795501d8acf7d70a4f8f9e30c5d629a2e0b410fb
navysigma <niorugarox@gmail.com>
2026-06-07 19:16:49 +0700

    fix commitnya eris

--- src/pages/user/Login.tsx
+++ src/pages/user/Login.tsx
@@ -1,6 +1,6 @@
-import { FormEvent, useState } from "react";
+import { useState, type FormEvent } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../../hooks/useAuth";
+import { useAuth } from "../../contexts/useAuth";
 
 export default function Login() {
   const { login } = useAuth();
--- src/pages/user/Register.tsx
+++ src/pages/user/Register.tsx
@@ -1,5 +1,5 @@
 import { useState, type FormEvent } from "react";
-import { useAuth } from "../../contexts/AuthContext";
+import { useAuth } from "../../contexts/useAuth";
 
 export default function Register() {
   const { register } = useAuth();
