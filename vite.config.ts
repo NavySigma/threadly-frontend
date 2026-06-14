038ace262dc7a6b31b67dee719b6d23716bb0c71
navysigma <niorugarox@gmail.com>
2026-06-15 00:37:17 +0700

    dekstop and pwa WIP

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,50 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Threadly",
+        short_name: "Threadly",
+        description: "Platform tanya jawab programming",
+        theme_color: "#111113",
+        icons: [
+          {
+            src: "/src/assets/threadly-removebg-preview.jpeg",
+            sizes: "192x192",
+            type: "image/jpeg",
+          },
+          {
+            src: "/src/assets/threadly-removebg-preview.jpeg",
+            sizes: "512x512",
+            type: "image/jpeg",
+          },
+        ],
+        shortcuts: [
+          {
+            name: "Threadly",
+            short_name: "Threadly",
+            description: "Buka Threadly",
+            url: "/",
+            icons: [
+              {
+                src: "/src/assets/threadly-removebg-preview.jpeg",
+                sizes: "192x192",
+                type: "image/jpeg",
+              },
+            ],
+          },
+        ],
+      },
+    }),
+  ],
+});
